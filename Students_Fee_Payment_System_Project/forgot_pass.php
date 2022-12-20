a98a30ffce84735e17f522f7d30338381a0fbb29
Rafi <76987718+Rafi0155@users.noreply.github.com>
2022-12-20 10:55:40 +0600

    Add files via upload

---
+++ Students_Fee_Payment_System_Project/forgot_pass.php
@@ -0,0 +1,41 @@
+<?php
+include 'forgot_pass.html';
+        $login_db =mysqli_connect("localhost", "root", "", "sfps_project" );
+
+        if(!$login_db)
+        {
+            echo "Database not connected";
+        }
+        else
+        {
+            echo "database connected" . "<br>" ;
+        }
+
+        if(isset($_POST['email'])){
+            $_email = $_POST['email'];
+            $_pass = $_POST['pswd'];
+
+            $sql_Select = "SELECT * FROM login_db 
+                            WHERE Email = '".$_email."'";
+
+            $result_Select = mysqli_query($login_db, $sql_Select);
+
+            if(mysqli_num_rows($result_Select)==1){
+
+                mysqli_query($login_db, "UPDATE login_db 
+                                        SET User_Password = '".$_pass."' 
+                                        WHERE Email = '".$_email."'");
+
+                echo "Reset Successfully<br>";
+                
+            }else{
+                echo "Reset Failed<br>";
+            }
+
+        }
+        else
+        {
+            echo "error" . mysqli_error($login_db);
+        }
+?>
+<a href="forgot_pass.html">Go back</a>;
---
+++ Students_Fee_Payment_System_Project/sign_up.php
@@ -0,0 +1,31 @@
+    <?php
+    include 'login.html';
+        $login_db =mysqli_connect("localhost", "root", "", "sfps_project" );
+
+        if(!$login_db)
+        {
+            echo "Database not connected";
+        }
+        else
+        {
+            echo "database connected" . "<br>" ;
+        }
+
+        $_name = $_POST['text'];
+        $_email = $_POST['email'];
+        $_pass = $_POST['pswd'];
+ 
+        $sql_Insert = "INSERT INTO login_db(Email, User_Name, User_Password) 
+                        VALUES ('$_email', '$_name', '$_pass')";
+        $result_Insert = mysqli_query($login_db, $sql_Insert);
+
+        if($result_Insert)
+        {
+            echo "Information Inserted <br>"; 
+        }
+        else
+        {
+            echo "error" . mysqli_error($login_db);
+        }
+    ?>
+    <a href="login.html">Go back</a>;
